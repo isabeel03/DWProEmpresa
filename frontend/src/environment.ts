e3bde66eb0cdd28a2de5b2898d388fc46e5a0868
isabeel03 <75174678@continental.edu.pe>
2026-06-25 20:31:04 -0500

    6.2

---
+++ frontend/src/environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.ts
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://zdvjkkwfsvsjlcgyrtxh.supabase.co',
+  supabaseKey: 'sb_publishable_8H7B84TImgmWHOON_Sp0gg_qQfKzKh2'
+};
